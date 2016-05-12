e6c9ddefbe0586c4369ead09a8a5e9e7bfe26731
Adam Horvath <adamhorvath88@yahoo.com>
2016-05-12 23:51:11 +0200

    Updated sample method

--- source/java/querybuilder/QueryBuilder.java
+++ source/java/querybuilder/QueryBuilder.java
@@ -1,5 +1,10 @@
 package querybuilder;
 
+import querybuilder.sql.SqlQuery;
+import querybuilder.sample.Celebrity;
+
+import java.util.Collection;
+
 /**
  * .
  * @author Adam
@@ -7,18 +12,24 @@ package querybuilder;
 public class QueryBuilder {
 
 	public void sample() {
-		Query allCelebs = Query.Select().from(Celebrity.class);
+		Query from = SqlQuery.Select().from(Celebrity.class);
 
-		Query celebsAges = Query.Select().distinct("age").from(Celebrity.class);
+		Query allCelebs = SqlQuery.Select().from(Celebrity.class);
 
-		Query whoIs22YO = Query.Select().count().from(Celebrity.class).where("age").equal(22);
+		System.out.println(allCelebs.toString());
 
-		Query addBieber = Query.Insert().into(Celebrity.class, "id", "name", "age").values(1, "Justin Bieber", 21);
+		Collection<Celebrity> celebs = allCelebs.execute();
 
-		Query celebNames = Query.Select("name").from(Celebrity.class);
+		Query celebsAges = SqlQuery.Select().distinct("age").from(Celebrity.class);
 
-		Query makeFirstCeleb22 = Query.Update(Celebrity.class).set("age", 22).where("id").equal(1);
+		Query whoIs22YO = SqlQuery.Select().count().from(Celebrity.class).where("age").equal(22);
 
-		Query deleteTwitterVirgins = Query.Delete().from(Celebrity.class).where("twitterHandle").isNull();
+		Query addBieber = SqlQuery.Insert().into(Celebrity.class, "id", "name", "age").values(1, "Justin Bieber", 21);
+
+		Query celebNames = SqlQuery.Select("name").from(Celebrity.class);
+
+		Query makeFirstCeleb22 = SqlQuery.Update(Celebrity.class).set("age", 22).where("id").equal(1);
+
+		Query deleteTwitterVirgins = SqlQuery.Delete().from(Celebrity.class).where("twitterHandle").isNull();
 	}
 }
